e7a28caf2b7e82f57d0e9789eef40de3e076e4b2
vergilium <vergilium2@gmail.com>
2025-11-10 20:02:38 +0200

    fix index.html and add sitemap

---
+++ src/app/components/root-page/root-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Home } from '../../pages/home/home';
+import { SeoService } from '../../services/seo.service';
+
+/**
+ * Root page component that renders Home component with default language
+ * This component is used for prerendering the root route '/'
+ */
+@Component({
+  selector: 'app-root-page',
+  standalone: true,
+  imports: [Home],
+  template: `<app-home></app-home>`,
+  styles: '',
+})
+export class RootPageComponent implements OnInit {
+  private readonly seoService = inject(SeoService);
+
+  ngOnInit(): void {
+    // Set SEO for root page (default language)
+    this.seoService.updateSeoFromConfig({
+      titleKey: 'seo.home.title',
+      descriptionKey: 'seo.home.description',
+      keywordsKey: 'seo.home.keywords',
+      imageKey: 'seo.home.image',
+      structuredDataKey: 'seo.home.structuredData',
+      type: 'website',
+    });
+  }
+}
